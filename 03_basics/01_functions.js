18a24f814aae9d60a2ae327abd1c23c5c363f143
MohammadAriz <141039478+MohammadAriz@users.noreply.github.com>
2024-05-23 21:15:59 +0000

    functions and scopes

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,66 @@
+function sayMyName(){
+console.log("A")
+console.log("r")
+console.log("i")
+console.log("z")
+}
+sayMyName()
+
+function addTwoNumbers(num1,num2){
+    
+    console.log(num1+num2);
+}
+const result=addTwoNumbers(9,5)
+addTwoNumbers(5,"a")
+console.log(result) //will give you undefined
+
+function addNumbers(num1,num2){
+    console.log("here will work because before return it works")
+    return num1+num2
+    console.log("here will not work because before return it wont works")
+}
+const resultReturn=addNumbers(10,5)
+console.log(resultReturn)
+
+
+function userLoginMessage(message){
+    if(message){
+        return `${message} is just logged in`
+    }
+    else{
+        return "please enter input"
+    }
+   
+}
+console.log(userLoginMessage("Ariz"))
+console.log(userLoginMessage("")) //if empty string passed 
+console.log(userLoginMessage()) //nothing is passed 
+
+function addCartItems(num){
+return num;
+}
+console.log(addCartItems(2))
+console.log(addCartItems(2,3,4))
+
+function addCartItemsAgain(...num){
+    return num;
+    }
+console.log(addCartItemsAgain(2,3,4))
+
+const user={
+    userName:"Ariz",
+    price:199
+}
+function object(anything){
+return `user name is ${anything.userName} and price is ${anything.price}`
+}
+console.log(object(user))
+console.log(object({
+    userName:"sam"
+}))
+
+const myNewArray=[200,400,600,800]
+function returnSecondValue(anything){
+console.log(anything[1])
+}
+returnSecondValue(myNewArray)
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,57 @@
+var a=10
+let b=20 
+const c= 30
+console.log(a)
+console.log(b)
+console.log(c)
+
+{
+var a=100
+let b=200
+const c=300
+}
+console.log(a)// not blocked scope 
+console.log(b) //this wont give you 200
+console.log(c)// this wont give you 300
+
+
+function one(){
+    const username="Ariz"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+   
+    two()
+    // console.log(website)
+}
+one()
+
+if(true){
+    const username="Ariz"
+    if(username==="Ariz"){
+        const website="youtube"
+        console.log(website + username)
+    }
+    // console.log(wrebsite) will throw an error
+}
+// console.log(username) also will throw an error
+
+
+
+addone(3) //here we can define before also
+function addone(num){
+console.log(num)
+}
+
+const addtwo= function(num){
+console.log(num)
+}
+addtwo(5) //here  we can define but see next code
+
+
+addthree(5) //here cant define earlier 
+const addthree= function(num){
+    console.log(num)
+    }
+   
