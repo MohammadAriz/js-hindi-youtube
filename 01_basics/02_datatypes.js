f832be2167dad577d3dedfde7b25945008f972db
MohammadAriz <141039478+MohammadAriz@users.noreply.github.com>
2024-07-05 07:33:19 +0000

    conversion and operations

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,4 +1,4 @@
 // number,bigInt, string,boolean,symbol,null,undefined - primitive data types
-//Array and Object - non primitive data
+//Array and Object - non primitive datatypes.
 // console.log(typeof Null) will give you object. 
 // console.log(typeof Undefined) will give you Undefined. 
---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,27 @@
+let age=33;
+// let age="33abc" //this will get converted to number but its value will be NaN
+
+let stringAge=Number(age);
+console.log(stringAge);
+console.log(typeof stringAge);
+
+let value= undefined
+// let value= null
+let newValue= Number(value);
+console.log(typeof newValue)
+console.log(newValue)
+
+//Notes- "33" will get convert to 33
+// "33abc" will give NaN
+//true will give 1 ,"" will give false but "written" will give true
+
+// ********OPERATIONS*********
+// let str1="hello";
+// let str2=" Ariz";
+// console.log(str1+str2);
+
+console.log(1+"2")//will  give you 12
+console.log("1"+2)//will  give you 12
+console.log("1"+2-13) //now will become 12-13
+console.log("1"+2+2)
+console.log(1+2+"2") //will give you 32
