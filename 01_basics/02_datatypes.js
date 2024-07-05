ce0277e4cf7126a2346a79ad2f1f1816ad5d4d45
MohammadAriz <141039478+MohammadAriz@users.noreply.github.com>
2024-07-05 06:40:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,4 @@
+// number,bigInt, string,boolean,symbol,null,undefined - primitive data types
+//Array and Object - non primitive data
+// console.log(typeof Null) will give you object. 
+// console.log(typeof Undefined) will give you Undefined. 
